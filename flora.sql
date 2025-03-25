bcf899821f4ebc5a01658cfaa877453cae264856
melpiress <melissa.foneca@germinare.org.br>
2025-03-25 12:40:51 -0300

    Adicionando as tabelas endereco e admin e removendo a tabela telefone

--- flora.sql
+++ flora.sql
@@ -1,19 +1,20 @@
-CREATE TABLE Cliente
+CREATE TABLE cliente
 (
-    ID INT PRIMARY KEY,
-    nome VARCHAR(30),
-    sobrenome VARCHAR(60),
-    endereco VARCHAR(200),
-    sexo VARCHAR(9),
-    nome_usuario VARCHAR(50),
-    foto_perfil VARCHAR(500)
+    id SERIAL PRIMARY KEY,
+    nome_completo VARCHAR(90),
+    email VARCHAR(50) UNIQUE,
+    nome_usuario VARCHAR(50) UNIQUE,
+    foto_perfil VARCHAR(500),
+    senha VARCHAR(30),
+    telefone VARCHAR(11)
+
 );
 
-CREATE TABLE Pedido
+CREATE TABLE pedido
 (
-    ID INT PRIMARY KEY,
-    ID_produto INT,
-    ID_cliente INT,
+    id SERIAL PRIMARY KEY,
+    id_produto INT,
+    id_cliente INT,
     data_pedido DATE,
     qnt_produto INT,
     forma_pgto VARCHAR(50),
@@ -23,39 +24,56 @@ CREATE TABLE Pedido
     idProduto INT
 );
 
-CREATE TABLE Produto
+CREATE TABLE produto
 (
     nome VARCHAR(50),
     preco_unid NUMERIC(10,2),
-    ID INT PRIMARY KEY,
-    ID_categoria INT,
+    id SERIAL PRIMARY KEY,
+    id_categoria INT,
     qnt_estoque INT
 );
 
-CREATE TABLE Categoria
+CREATE TABLE categoria
 (
-    ID INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nome VARCHAR(50)
 );
 
-CREATE TABLE Cartao
+CREATE TABLE cartao
 (
     numero VARCHAR(16) PRIMARY KEY,
-    ID_cliente INT,
+    id_cliente INT,
     CVV VARCHAR(3),
     validade DATE,
-    nome_cartao VARCHAR(50)
+    nome_cartao VARCHAR(50),
+    apelido_cartao VARCHAR(40)
 );
 
-CREATE TABLE Telefone
+CREATE TABLE endereco
 (
+    id SERIAL PRIMARY KEY,
+    id_cliente INT,
+    cep VARCHAR(9),
     numero INT,
-    ID INT PRIMARY KEY,
-    ID_cliente INT
+    logradouro VARCHAR(50),
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    estado VARCHAR(50),
+    ponto_referencia VARCHAR(30)
 );
 
-ALTER TABLE Pedido ADD FOREIGN KEY(ID_cliente) REFERENCES Cliente (ID);
-ALTER TABLE Pedido ADD FOREIGN KEY(ID_produto) REFERENCES Produto (ID);
-ALTER TABLE Produto ADD FOREIGN KEY(ID_categoria) REFERENCES Categoria (ID);
-ALTER TABLE Cartao ADD FOREIGN KEY(ID_cliente) REFERENCES Cliente (ID);
-ALTER TABLE Telefone ADD FOREIGN KEY(ID_cliente) REFERENCES Cliente (ID);
+CREATE TABLE admin
+(
+    id SERIAL PRIMARY KEY,
+    admin_name VARCHAR UNIQUE,
+    email VARCHAR(50) UNIQUE,
+    senha VARCHAR(50)
+);
+
+ALTER TABLE pedido ADD FOREIGN KEY(id_cliente) REFERENCES cliente (id);
+ALTER TABLE pedido ADD FOREIGN KEY(id_produto) REFERENCES produto (id);
+ALTER TABLE produto ADD FOREIGN KEY(id_categoria) REFERENCES categoria (id);
+ALTER TABLE cartao ADD FOREIGN KEY(id_cliente) REFERENCES cliente (id);
+ALTER TABLE endereco ADD FOREIGN KEY(id_cliente) REFERENCES cliente (id);
+
+
